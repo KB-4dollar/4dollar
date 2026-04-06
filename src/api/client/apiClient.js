2f2ee6fd7c3613a6ec5017426a1dfa6419dd7ebc
ChaeSooYeon <cotndus3@gmail.com>
2026-04-06 14:08:09 +0900

    core: json-server railway 설정

--- src/api/client/apiClient.js
+++ src/api/client/apiClient.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
-
+const API_URL = import.meta.env.VITE_API_URL; //railway server URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; //기본 json-server URL
 /**
  * 기본 API 인스턴스 생성
  */
 const apiClient = axios.create({
   // 환경변수로 관리하는 것을 권장하며, 기본값으로 json-server 주소 세팅
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
+  baseURL: API_URL || 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
   },
   timeout: 5000,
 });
 
-
 apiClient.interceptors.request.use(
   (config) => {
     // TODO: [F-00-2] 토큰 주입 로직
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 apiClient.interceptors.response.use(
@@ -26,7 +26,7 @@ apiClient.interceptors.response.use(
   (error) => {
     // TODO: [F-04-1, F-00-2] 에러 및 토큰 만료 처리
     return Promise.reject(error);
-  }
+  },
 );
 
 export default apiClient;
