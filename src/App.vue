d4e27fa555ba8178995b2efbf536dcde787e7944
shinhyung <23102009@seoultech.ac.kr>
2026-04-07 22:00:25 +0900

    fix: revert error

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <script setup>
 import axios from 'axios';
-// const requestAPI = () => {
-//   // const url = "http://localhost:3000/users 와 같음";
-//   const url = '/api/users';
-//   axios.get(url).then((response) => {
-//     console.log('# 응답객체 : ', response);
-//   });
-// };
-// requestAPI();
-
-const API_URL = import.meta.env.VITE_API_URL;
-
 const requestAPI = () => {
-  axios.get(`${API_URL}/users`).then((response) => {
+  // const url = "http://localhost:3000/users 와 같음";
+  const url = '/api/users';
+  axios.get(url).then((response) => {
     console.log('# 응답객체 : ', response);
   });
 };
