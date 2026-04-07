6d3dcf94134c998be24936f0f98a3822c38bd651
ChaeSooYeon <cotndus3@gmail.com>
2026-04-07 14:49:49 +0900

    refactor: 파일명 변경

--- src/pages/TransactionDetailPage.vue
+++ src/pages/TransactionDetailPage.vue
@@ -1,11 +1,11 @@
 <script setup>
-import TransactionForm from '@/components/transaction/TransactionForm.vue';
+import TransactionDetailForm from '@/components/transaction/TransactionDetailForm.vue';
 import TransactionDetail from '@/components/transaction/TransactionDetail.vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 </script>
 <template>
-  <TransactionForm v-if="route.params.id === 'new' || !route.params.id" />
+  <TransactionDetailForm v-if="route.params.id === 'new' || !route.params.id" />
   <TransactionDetail v-else />
 </template>
