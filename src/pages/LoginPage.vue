ee2824a78f913b0652a946ab44bde32a786043ef
shinhyung <23102009@seoultech.ac.kr>
2026-04-07 11:43:40 +0900

    feat: 로그인 회원가입 template 구현

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,4 +1,5 @@
 <script setup>
+import axios from 'axios';
 import router from '@/router';
 import { useAuthStore } from '@/stores/auth';
 const authStore = useAuthStore();
@@ -8,7 +9,28 @@ const login = () => {
 };
 </script>
 <template>
-  <h1>Login</h1>
-  <button @click="login">[로그인]</button>
+  <div class="login">
+    <h1 class="text-2xl font-bold">로그인</h1>
+
+    <div class="form-group">
+      <label>이메일</label>
+      <input type="email" />
+    </div>
+
+    <div class="form-group">
+      <label>비밀번호</label>
+      <input type="password" />
+    </div>
+
+    <button @onClink="login">로그인</button>
+
+    <div class="signup-link">
+      <RouterLink to="/signup">회원가입</RouterLink>
+    </div>
+  </div>
 </template>
-<style scoped></style>
+<style scoped>
+.login {
+  width: 448px;
+}
+</style>
---
+++ src/pages/SignupPage.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+<template>
+  <div class="signup">
+    <h1 class="text-2xl font-bold">회원가입</h1>
+
+    <div class="form-group">
+      <label>이름</label>
+      <input type="text" />
+    </div>
+
+    <div class="form-group">
+      <label>이메일</label>
+      <input type="email" />
+    </div>
+
+    <div class="form-group">
+      <label>비밀번호</label>
+      <input type="password" />
+    </div>
+
+    <div class="form-group">
+      <label>비밀번호 확인</label>
+      <input type="password" />
+    </div>
+
+    <button>회원가입</button>
+  </div>
+</template>
+<style scoped></style>
