da9711fd19090381c8f66c158ce0ace1d6d017ed
ChaeSooYeon <cotndus3@gmail.com>
2026-04-07 15:31:57 +0900

    chore: 섹션 카드 공통 컴포넌트 추가

--- src/pages/TransactionDetailPage.vue
+++ src/pages/TransactionDetailPage.vue
@@ -1,11 +1,11 @@
 <script setup>
-import TransactionDetailForm from '@/components/transaction/TransactionDetailForm.vue';
+import TransactionForm from '@/components/transaction/TransactionForm.vue';
 import TransactionDetail from '@/components/transaction/TransactionDetail.vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 </script>
 <template>
-  <TransactionDetailForm v-if="route.params.id === 'new' || !route.params.id" />
+  <TransactionForm v-if="route.params.id === 'new' || !route.params.id" />
   <TransactionDetail v-else />
 </template>
