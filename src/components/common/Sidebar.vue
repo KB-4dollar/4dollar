715eb36ebd6c483f11daf890aaec93d4fdae565a
ShinHyung Park <161359299+shinh09@users.noreply.github.com>
2026-04-08 19:43:51 +0900

    * style: 레이아웃 스타일 수정 (#38)
    * style: 레이아웃 스타일 수정
    
    * style: 사이드바 반응형 구현

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -4,14 +4,14 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 
 const menus = [
-  { name: '대시보드', path: '/' },
+  { name: '대시보드', path: '/app' },
   { name: '거래내역', path: '/list' },
   { name: '입출금', path: '/transaction/new' },
 ];
 </script>
-
 <template>
-  <aside class="w-60 bg-gray-100 p-5 flex flex-col">
+  <!-- ✅ PC용 사이드바 -->
+  <aside class="hidden md:flex w-60 bg-white p-5 flex-col">
     <nav class="flex flex-col gap-3">
       <RouterLink
         v-for="menu in menus"
@@ -28,4 +28,19 @@ const menus = [
       </RouterLink>
     </nav>
   </aside>
+
+  <!-- ✅ 모바일 하단 네비 (밖으로 빼기) -->
+  <nav
+    class="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 h-16 flex justify-around items-center md:hidden"
+  >
+    <RouterLink
+      v-for="menu in menus"
+      :key="menu.path"
+      :to="menu.path"
+      class="text-sm"
+      :class="route.path === menu.path ? 'text-[#d97a5f]' : 'text-gray-500'"
+    >
+      {{ menu.name }}
+    </RouterLink>
+  </nav>
 </template>
